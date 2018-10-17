9b7e631c99a6cad730e9b87b8804d74988d813e3
Ricardo Rainha <ricardofrainha@gmail.com>
2018-10-17 14:59:18 -0300

    Adding Movie and MoviesList models
    Signed-off-by: Ricardo Rainha <ricardofrainha@gmail.com>

---
+++ app/src/main/java/com/ricardorainha/famousmovies/models/MoviesList.java
@@ -0,0 +1,54 @@
+package com.ricardorainha.famousmovies.models;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class MoviesList {
+
+    @SerializedName("page")
+    @Expose
+    private int page;
+    @SerializedName("total_results")
+    @Expose
+    private int totalResults;
+    @SerializedName("total_pages")
+    @Expose
+    private int totalPages;
+    @SerializedName("movies")
+    @Expose
+    private List<Movie> movies = null;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+}
