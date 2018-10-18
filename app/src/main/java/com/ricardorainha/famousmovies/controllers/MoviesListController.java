9f306843d62fd8c47ceccfcdd552df41137acd60
Ricardo Rainha <ricardofrainha@gmail.com>
2018-10-18 18:33:05 -0300

    Adding request types to MoviesListController
    Signed-off-by: Ricardo Rainha <ricardofrainha@gmail.com>

--- app/src/main/java/com/ricardorainha/famousmovies/controllers/MoviesListController.java
+++ app/src/main/java/com/ricardorainha/famousmovies/controllers/MoviesListController.java
@@ -13,30 +13,36 @@ public class MoviesListController extends Observable implements Callback<MoviesL
 
     private TheMovieDB.Endpoints moviesAPI;
     private MoviesList moviesList;
+    private RequestType requestType;
 
     public static int RESPONSE_SUCCESS = 999;
     public static int RESPONSE_FAILED = 888;
+    public static int REQUEST_FAILURE = 777;
 
     public MoviesListController() {
         moviesAPI = TheMovieDB.getAPI();
     }
 
-    public void requestPopularMovies() {
+    public void requestMovies(RequestType type) {
         if (moviesAPI != null) {
-            Call<MoviesList> moviesCall = moviesAPI.getPopularMovies();
-            moviesCall.enqueue(this);
-        }
-    }
-
-    public void requestTopRatedMovies() {
-        if (moviesAPI != null) {
-            Call<MoviesList> moviesCall = moviesAPI.getTopRatedMovies();
+            requestType = type;
+            Call<MoviesList> moviesCall;
+            if (requestType == RequestType.MOST_POPULAR) {
+                moviesCall = moviesAPI.getPopularMovies();
+            }
+            else {
+                moviesCall = moviesAPI.getTopRatedMovies();
+            }
             moviesCall.enqueue(this);
         }
     }
 
     public MoviesList getMoviesList() {
         return moviesList;
+    }
+
+    public RequestType getRequestType() {
+        return requestType;
     }
 
     @Override
@@ -56,5 +62,26 @@ public class MoviesListController extends Observable implements Callback<MoviesL
     @Override
     public void onFailure(Call<MoviesList> call, Throwable t) {
         t.printStackTrace();
+        setChanged();
+        notifyObservers(REQUEST_FAILURE);
+    }
+
+    public enum RequestType {
+        MOST_POPULAR("Most Popular Movies"),
+        TOP_RATED("Top Rated Movies");
+
+        private final String title;
+
+        RequestType(String t) {
+            title = t;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String toString() {
+            return getTitle();
+        }
     }
 }
