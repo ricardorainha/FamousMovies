5bedf12931faed6f45b3d0d40e7e035c0bad4550
Ricardo Rainha <ricardofrainha@gmail.com>
2018-10-20 17:32:39 -0300

    Updating layout
    - Added RecyclerView and ProgressBar to the layout
    - Added Warning message and icon
    - Included warning icon resource
    
    Signed-off-by: Ricardo Rainha <ricardofrainha@gmail.com>

--- app/src/main/java/com/ricardorainha/famousmovies/MainActivity.java
+++ app/src/main/java/com/ricardorainha/famousmovies/MainActivity.java
@@ -2,6 +2,10 @@ package com.ricardorainha.famousmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.ricardorainha.famousmovies.controllers.MoviesListController;
@@ -12,11 +16,16 @@ import java.util.Observer;
 public class MainActivity extends AppCompatActivity implements Observer {
 
     private MoviesListController controller;
+    private RecyclerView rvMovies;
+    private ProgressBar pbLoading;
+    private FrameLayout flWarning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configureViews();
 
         controller = new MoviesListController();
         controller.addObserver(this);
@@ -37,8 +46,15 @@ public class MainActivity extends AppCompatActivity implements Observer {
                 Toast.makeText(this, "There was a problem with your request. Please check your internet connection.", Toast.LENGTH_LONG).show();
             }
 
+            showWarningMessage(result != MoviesListController.RESPONSE_SUCCESS);
             showProgressBar(false);
         }
+    }
+
+    private void configureViews() {
+        rvMovies = (RecyclerView) findViewById(R.id.rv_movies);
+        pbLoading = (ProgressBar) findViewById(R.id.pb_loading);
+        flWarning = (FrameLayout) findViewById(R.id.fl_warning);
     }
 
     private void requestMovies(MoviesListController.RequestType requestType) {
@@ -47,7 +63,12 @@ public class MainActivity extends AppCompatActivity implements Observer {
     }
 
     private void showProgressBar(boolean show) {
+        pbLoading.setVisibility(show ? View.VISIBLE : View.GONE);
+    }
 
+    private void showWarningMessage(boolean show) {
+        flWarning.setVisibility(show ? View.VISIBLE : View.GONE);
+        rvMovies.setVisibility(show ? View.GONE : View.VISIBLE);
     }
 
 }
